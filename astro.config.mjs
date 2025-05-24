ae470feba135b71f31c92c6defec72fa51ff12cc
gvoze32 <gvoze32@gmail.com>
2025-05-24 13:02:51 +0700

    Integrate @astrojs/node adapter for standalone deployment and update package dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
+  adapter: node({
+    mode: 'standalone'
+  }),
   vite: {
     plugins: [tailwindcss()]
   },
-
-  integrations: [react()]
+  integrations: [react()],
 });
